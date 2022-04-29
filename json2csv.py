cbefa2890965fb16986623f04040f390cd565911
akifgas <71625965+akifgas@users.noreply.github.com>
2022-04-30 01:58:36 +0200

    Add files via upload

--- json2csv.py
+++ json2csv.py
@@ -22,9 +22,9 @@ def vrd_dicts(path):
                
                 "subject_bbox": {"x":anno["subject"]["x"], "y":anno["subject"]["y"], "w":anno["subject"]["w"], "h":anno["subject"]["h"]},
                 "object_bbox": {"x":anno["object"]["x"], "y":anno["object"]["y"], "w":anno["object"]["w"], "h":anno["object"]["h"]},
-                "object":  anno["object"]["name"],
-                "subject":  anno["subject"]["name"],
-                "predicate": anno["predicate"]
+                "object":  {anno["object"]["name"]},
+                "subject":  {anno["subject"]["name"]},
+                "predicate": {anno["predicate"]}
             }
            
             objs.append(obj)
@@ -54,3 +54,4 @@ f.close()
 df = pd.json_normalize(json_data, record_path=["relationships"], meta=["image_id"])
 df.to_csv("/truba_scratch/agasi/Dataset/relationships.csv",index = False, header = False, sep=";")
 
+df
